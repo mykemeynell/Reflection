07c84c5eb3e9de6faaa872d468b02c0417592593
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2026-08-27 16:13:24 +0100

    chore: remove redundant `function_exists` check for `app` helper function in `helpers.php`

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -7,22 +7,20 @@ namespace mykemeynell\Reflection\Helpers;
 use Closure;
 use mykemeynell\Reflection\Application\Container;
 
-if (! function_exists('app')) {
-    /**
-     * Resolve and return a service from the container or return the container instance itself.
-     *
-     * @param  string|Closure|null  $abstract  The abstract type or closure to resolve. If null, the container instance is returned.
-     * @param  mixed  ...$parameters  Additional parameters to pass to the resolution process.
-     * @return mixed The resolved service instance or the container instance.
-     */
-    function app(string|Closure|null $abstract = null, ...$parameters): mixed
-    {
-        if (count($parameters) === 1 && isset($parameters[0]) && is_array($parameters[0])) {
-            $parameters = $parameters[0];
-        }
-
-        return ($container = Container::getInstance()) && $abstract !== null
-            ? $container->make($abstract, $parameters)
-            : $container;
+/**
+ * Resolve and return a service from the container or return the container instance itself.
+ *
+ * @param  string|Closure|null  $abstract  The abstract type or closure to resolve. If null, the container instance is returned.
+ * @param  mixed  ...$parameters  Additional parameters to pass to the resolution process.
+ * @return mixed The resolved service instance or the container instance.
+ */
+function app(string|Closure|null $abstract = null, ...$parameters): mixed
+{
+    if (count($parameters) === 1 && isset($parameters[0]) && is_array($parameters[0])) {
+        $parameters = $parameters[0];
     }
+
+    return ($container = Container::getInstance()) && $abstract !== null
+        ? $container->make($abstract, $parameters)
+        : $container;
 }
